dd1d9ad4399af48342c684d8f631099ef07cf663
Vanessa <vanessa.zhxx@gmail.com>
2022-07-19 12:04:51 +0100

    Reorganise stock.h

--- Stock.cpp
+++ Stock.cpp
@@ -1,1 +1,18 @@
 #include "Stock.h"
+
+Stock::Stock(float*& data, float s0, float mu, float var, int t, int x) {
+	this->s0 = s0;
+	this->mu = mu;
+	this->var = var;
+	this->t = t;
+	this->x = x;
+
+	this->rn = data;
+}
+
+float Stock::price_stock(int offset) {
+	float price = s0 * exp((mu - 0.5f * var * var) * t
+		+ var * sqrtf(t) * rn[offset]);
+
+	return price * x;
+}
--- Stock.h
+++ Stock.h
@@ -1,25 +1,11 @@
 #pragma once
 #include<math.h>
 
-class Stock
-{
+class Stock{
+
 public:
-	Stock(float*& data, float s0, float mu, float var, int t, int x) {
-		this->s0 = s0;
-		this->mu = mu;
-		this->var = var;
-		this->t = t;
-		this->x = x;
-
-		this->rn = data;
-	}
-
-	float price_stock(int offset) {
-		float price = s0 * exp((mu - 0.5f * var * var) * t 
-						+ var * sqrtf(t) * rn[offset]);
-
-		return price * x;
-	};
+	Stock(float*& data, float s0, float mu, float var, int t, int x);
+	float price_stock(int offset);
 private:
 	float s0;
 	float mu;
